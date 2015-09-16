26202853497fa3be46dc3963afe30b8899902ea7
Xiangliang Meng <meng.xiangliang1985@gmail.com>
2015-09-16 19:40:24 +0900

    bugfix

--- redis.c
+++ redis.c
@@ -8,16 +8,15 @@
 #include <mysql.h>
 #include <string.h>
 #include <hiredis.h>
-#define UNIXSOCKET "/tmp/redis.sock"
+#define UNIXSOCKET "/var/tmp/redis.sock"
 
 my_bool redis_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
 
     redisContext *rdsCon;
-
-    if(args->arg_count != 1){
-        strcpy(message, "redis() requires one argument");
+    if(args->arg_count!=1 || args->lengths[0]==0 || args->arg_type[0]!=STRING_RESULT || args->args[0]==NULL || args->args[0][0]==' '){
+        strcpy(message, "redis() requires ONE string arguments");
         return 1;
-    }
+    }   
 
     initid->maybe_null = 1;
     initid->ptr = NULL;
@@ -28,7 +27,7 @@ my_bool redis_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
         redisFree(rdsCon);
         strcpy(message, "redis() connect to db failed");
         return 1;
-    }
+    }   
 
     initid->ptr = (char*)rdsCon;
     return 0;
@@ -39,75 +38,61 @@ void redis_deinit(UDF_INIT *initid) {
         redisContext *rdsCon;
         rdsCon = (redisContext*)initid->ptr;
         redisFree(rdsCon);
-    }
+    }   
 }
 
 char *redis(UDF_INIT *initid, UDF_ARGS *args,
     char *result, unsigned long *length,
     char *is_null, char *error)
 {
-    redisContext* rdsCon;
-    redisReply *rdsRply;
-    char* command;
-    unsigned long cmdLen;
-
+    redisContext* rdsCon = (redisContext*)initid->ptr;
     result[0] = '\0';
     *length = 0;
+    redisReply *rdsRply = redisCommand(rdsCon, args->args[0]);
 
-    // Connection
-    rdsCon = (redisContext*)initid->ptr;
-
-    // Create Null-terminated string
-    cmdLen = args->lengths[0];
-    if(cmdLen==0){
+    if(!rdsRply){
         *is_null=1;
-        return result;
-    }
-    command = strdup(args->args[0]);
+    }else{
+        switch(rdsRply->type) {
+            case REDIS_REPLY_ERROR:
+                *error = 1;
+                break;
 
-    // Exec command
-    rdsRply = redisCommand(rdsCon, command);
-    free(command);
+            case REDIS_REPLY_ARRAY: {
+                int elmLen;
+                int bufIdx = 0;
+                int i;
 
-    switch(rdsRply->type) {
-        case REDIS_REPLY_ERROR:
-            *error = 1;
-            break;
+                for(i=0; i<rdsRply->elements; i++){
+                    elmLen = rdsRply->element[i]->len;
+                    memcpy(&result[bufIdx], rdsRply->element[i]->str, elmLen);
+                    result[bufIdx + elmLen] = '\n';
+                    bufIdx += (elmLen + 1);
+                }
 
-        case REDIS_REPLY_ARRAY: {
-            int elmLen;
-            int bufIdx = 0;
-            int i;
-
-            for(i=0; i<rdsRply->elements; i++){
-                elmLen = rdsRply->element[i]->len;
-                memcpy(&result[bufIdx], rdsRply->element[i]->str, elmLen);
-                result[bufIdx + elmLen] = '\n';
-                bufIdx += (elmLen + 1);
+                result[bufIdx - 1] = '\0';
+                *length = bufIdx - 1;
+                break;
             }
+            case REDIS_REPLY_STRING:
+            case REDIS_REPLY_STATUS:
+                if(rdsRply->len > 0){
+                    strcpy(result, rdsRply->str);
+                    *length = rdsRply->len;
+                }else{
+                    *is_null = 1;
+                }
+                break;
 
-            result[bufIdx - 1] = '\0';
-            *length = bufIdx - 1;
-            break;
-        }
-        case REDIS_REPLY_STRING:
-        case REDIS_REPLY_STATUS:
-            if(rdsRply->len > 0){
-                strcpy(result, rdsRply->str);
-                *length = rdsRply->len;
-            }else{
+            case REDIS_REPLY_INTEGER:
+                sprintf(result, "%lld", rdsRply->integer);
+                *length = strlen(result);
+                break;
+
+            case REDIS_REPLY_NIL:
                 *is_null = 1;
-            }
-            break;
-
-        case REDIS_REPLY_INTEGER:
-            sprintf(result, "%lld", rdsRply->integer);
-            *length = strlen(result);
-            break;
-
-        case REDIS_REPLY_NIL:
-            *is_null = 1;
-            break;
+                break;
+        }
     }
 
     freeReplyObject(rdsRply);
